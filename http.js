305e88d28406576e0739c003da263b51ba316ebd
Alvaro Capaceta Magallanes <2020030170@upsin.edu.mx>
2023-02-24 22:16:17 -0700

    HTTP request

--- http.js
+++ http.js
@@ -1,10 +1,24 @@
 const http = require("http");
 
-http
-  .createServer((request, response) => {
-    response.write("Hello world");
-    response.end();
-  })
-  .listen(3000);
+const server = http.createServer((request, response) => {
+  if (request.url === "/") {
+    response.write("Welcome to Server");
+    return response.end();
+  }
+
+  if (request.url === "/about") {
+    response.write("Welcome to about");
+    return response.end();
+  }
+
+  response.write(`
+    <h1>Page not found 404</h1>
+    <p>Esta pagina no se encontro.</p>
+    <a href='/'>Volver a la pagina principal</a>
+    `);
+  response.end();
+});
+
+server.listen(3000);
 
 console.log("Servidor escuchando en el puerto 3000");
